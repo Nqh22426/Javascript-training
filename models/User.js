c7178aa08aefc2c9d5446c3cc05d3127f5da6f76
Nqh22426 <quanghungsv2225@gmail.com>
2025-11-04 21:37:13 +0700

    Update User Model for TODO-API

---
+++ models/User.js
@@ -0,0 +1,47 @@
+// User Model
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
+// Middleware: Hash password trước khi lưu
+userSchema.pre('save', async function(next) {
+  // Nếu password không thay đổi thì skip
+  if (!this.isModified('password')) return next();
+  
+  // Hash password với bcrypt (10 rounds)
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// So sánh password khi login
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
