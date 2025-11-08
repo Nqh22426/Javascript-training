d31862cf712e58eea20c4c479a72184fdb49e49c
Nqh22426 <quanghungsv2225@gmail.com>
2025-11-08 13:48:35 +0700

    Update main app & route for TODO-API

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Login from './components/Login';
+import Register from './components/Register';
+import TodoPage from './components/TodoPage';
+import PrivateRoute from './components/PrivateRoute';
+import './styles.css';
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route 
+            path="/" 
+            element={
+              <PrivateRoute>
+                <TodoPage />
+              </PrivateRoute>
+            } 
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
